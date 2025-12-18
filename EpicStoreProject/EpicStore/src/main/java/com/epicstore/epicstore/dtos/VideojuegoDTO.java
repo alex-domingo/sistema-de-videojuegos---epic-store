a05ca21a467766d4b937b40a4f17b1ca7f0e4c82
alex-domingo <domingoartola09@outlook.es>
2025-12-17 21:26:55 -0600

    mostramos listado de tienda desde nuestra db en un json desde insomnia

---
+++ EpicStoreProject/EpicStore/src/main/java/com/epicstore/epicstore/dtos/VideojuegoDTO.java
@@ -0,0 +1,77 @@
+package com.epicstore.epicstore.dtos;
+
+public class VideojuegoDTO {
+
+    private int idVideojuego;
+    private String titulo;
+    private double precio;
+    private String imagenPortada;
+    private String empresa;
+    private String clasificacion;
+    private int edadMinima;
+    private double calificacionPromedio;
+
+    public int getIdVideojuego() {
+        return idVideojuego;
+    }
+
+    public void setIdVideojuego(int idVideojuego) {
+        this.idVideojuego = idVideojuego;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getImagenPortada() {
+        return imagenPortada;
+    }
+
+    public void setImagenPortada(String imagenPortada) {
+        this.imagenPortada = imagenPortada;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(String clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public int getEdadMinima() {
+        return edadMinima;
+    }
+
+    public void setEdadMinima(int edadMinima) {
+        this.edadMinima = edadMinima;
+    }
+
+    public double getCalificacionPromedio() {
+        return calificacionPromedio;
+    }
+
+    public void setCalificacionPromedio(double calificacionPromedio) {
+        this.calificacionPromedio = calificacionPromedio;
+    }
+}
---
+++ EpicStoreProject/EpicStore/src/main/java/com/epicstore/epicstore/services/VideojuegoService.java
@@ -0,0 +1,15 @@
+package com.epicstore.epicstore.services;
+
+import com.epicstore.epicstore.dtos.VideojuegoDTO;
+import com.epicstore.epicstore.models.VideojuegoModel;
+
+import java.util.ArrayList;
+
+public class VideojuegoService {
+
+    private final VideojuegoModel videojuegoModel = new VideojuegoModel();
+
+    public ArrayList<VideojuegoDTO> obtenerVideojuegos() {
+        return videojuegoModel.listarVideojuegos();
+    }
+}
