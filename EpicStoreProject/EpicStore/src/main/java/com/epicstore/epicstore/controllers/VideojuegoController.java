ee88ec6742661c8973061defc95188d2644ddc1d
alex-domingo <domingoartola09@outlook.es>
2025-12-17 22:32:27 -0600

    integramos detalles de un videojuego en especifico

--- EpicStoreProject/EpicStore/src/main/java/com/epicstore/epicstore/controllers/VideojuegoController.java
+++ EpicStoreProject/EpicStore/src/main/java/com/epicstore/epicstore/controllers/VideojuegoController.java
@@ -1,6 +1,7 @@
 package com.epicstore.epicstore.controllers;
 
 import com.epicstore.epicstore.dtos.VideojuegoDTO;
+import com.epicstore.epicstore.dtos.VideojuegoDetalleDTO;
 import com.epicstore.epicstore.services.VideojuegoService;
 import com.google.gson.Gson;
 
@@ -24,21 +25,47 @@ public class VideojuegoController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        ArrayList<VideojuegoDTO> videojuegos = videojuegoService.obtenerVideojuegos();
-
-        HashMap<String, Object> salida = new HashMap<>();
-        salida.put("exito", true);
-        salida.put("mensaje", "Listado de videojuegos disponibles");
-        salida.put("total", videojuegos.size());
-        salida.put("datos", videojuegos);
+        String idParam = request.getParameter("id");
 
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
 
+        HashMap<String, Object> salida = new HashMap<>();
+        Gson gson = new Gson();
+
         try (PrintWriter out = response.getWriter()) {
-            Gson gson = new Gson();
+
+            if (idParam == null) {
+                // LISTADO
+                ArrayList<VideojuegoDTO> videojuegos = videojuegoService.obtenerVideojuegos();
+                salida.put("exito", true);
+                salida.put("mensaje", "Listado de videojuegos disponibles");
+                salida.put("total", videojuegos.size());
+                salida.put("datos", videojuegos);
+            } else {
+                // DETALLE
+                int idVideojuego = Integer.parseInt(idParam);
+                VideojuegoDetalleDTO detalle = videojuegoService.obtenerDetalle(idVideojuego);
+
+                if (detalle == null) {
+                    response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                    salida.put("exito", false);
+                    salida.put("mensaje", "Videojuego no encontrado");
+                } else {
+                    salida.put("exito", true);
+                    salida.put("mensaje", "Detalle del videojuego");
+                    salida.put("datos", detalle);
+                }
+            }
+
             out.print(gson.toJson(salida));
             out.flush();
+
+        } catch (NumberFormatException e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            salida.put("exito", false);
+            salida.put("mensaje", "El parámetro id debe ser numérico");
         }
     }
+
 }
