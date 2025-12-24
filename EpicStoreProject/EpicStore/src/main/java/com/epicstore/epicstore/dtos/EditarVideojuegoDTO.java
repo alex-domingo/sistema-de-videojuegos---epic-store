78cc2891542c198aed000504fedbeab9e341efa3
alex-domingo <domingoartola09@outlook.es>
2025-12-24 16:29:16 -0600

    editamos videojuego satisfactoriamente

---
+++ EpicStoreProject/EpicStore/src/main/java/com/epicstore/epicstore/dtos/EditarVideojuegoDTO.java
@@ -0,0 +1,68 @@
+package com.epicstore.epicstore.dtos;
+
+public class EditarVideojuegoDTO {
+
+    private Integer idClasificacion;
+    private String titulo;
+    private String descripcion;
+    private Double precio;
+    private String requisitosMinimos;
+    private String fechaLanzamiento; // "yyyy-MM-dd" o null
+    private String imagenPortada;
+
+    public Integer getIdClasificacion() {
+        return idClasificacion;
+    }
+
+    public void setIdClasificacion(Integer idClasificacion) {
+        this.idClasificacion = idClasificacion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getRequisitosMinimos() {
+        return requisitosMinimos;
+    }
+
+    public void setRequisitosMinimos(String requisitosMinimos) {
+        this.requisitosMinimos = requisitosMinimos;
+    }
+
+    public String getFechaLanzamiento() {
+        return fechaLanzamiento;
+    }
+
+    public void setFechaLanzamiento(String fechaLanzamiento) {
+        this.fechaLanzamiento = fechaLanzamiento;
+    }
+
+    public String getImagenPortada() {
+        return imagenPortada;
+    }
+
+    public void setImagenPortada(String imagenPortada) {
+        this.imagenPortada = imagenPortada;
+    }
+}
